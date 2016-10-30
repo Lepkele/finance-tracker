56845e0c819b82d2bf144fa536e48c7b4e90496e
Lepkele <charlessport2000@yahoo.com>
2016-10-30 07:59:12 +0000

    Add friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
   
  # get 'welcome/index', to: 'welcome#index'
 #  get 'welcome/about', to: 'welcome#about'
